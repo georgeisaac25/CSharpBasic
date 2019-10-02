e14d41448bdc5d58df21d158bd1345535b1a61bd
george.issac <george.isaac25@gmail.com>
2019-10-02 14:50:10 +0530

    use of delegate added

---
+++ CSharp/DelegateLearnPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CSharp
+{
+    public delegate bool IsEligibleDelegate(Employee employee);
+
+    public delegate int AddD(int a, int b);
+
+
+    public partial class DelegateLearnPage : ContentPage
+    {
+        public DelegateLearnPage()
+        {
+            InitializeComponent();
+
+            Employee employee1 = new Employee(10,"Designer");
+            Employee employee2 = new Employee(100, "Architect");
+
+            //in normal method call.
+            employee1.IsEligibleForPromotionBasedOnSalary(employee1);
+
+            
+            // we can pass the method in the form of delegate.
+            employee1.IsEligibleForPromotion(employee1, delegate (Employee employee)
+            {
+                return employee.designation.Equals("Architect");
+            });
+
+
+            employee2.IsEligibleForPromotion(employee2, delegate (Employee employee)
+            {
+                return employee.salary > 1;
+            });
+
+
+
+
+        }
+    }
+}
---
+++ CSharp/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace CSharp
+{
+    public class Employee
+    {
+        public int salary;
+        public string designation;
+
+        public Employee(int salary,string designation)
+        {
+            this.salary = salary;
+            this.designation = designation;
+        }
+
+        public bool IsEligibleForPromotion(Employee employee,IsEligibleDelegate isEligibleDelegate)
+        {
+            return isEligibleDelegate.Invoke(employee);
+        }
+
+        //public bool IsEligibleForPromotionBasedOnSalary(Employee employee)
+        //{
+        //    return employee.salary>1000;
+        //}
+
+        //public bool IsEligibleForPromotionBasedOnDesignation(Employee employee)
+        //{
+        //    return employee.designation.Equals("Architect");
+        //}
+
+    }
+}
